86d8b1fb35a6878306a5bcfdf21f3a9b63289865
Hardik203 <153900500+Hardik203@users.noreply.github.com>
2024-10-22 19:29:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./screen/Home"
+import Pred from "./screen/Pred";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Main />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home/>}></Route>
+        <Route path="/pred" element={<Pred/>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
