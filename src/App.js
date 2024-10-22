554f1460cfcdcba00b56ab0a5fa99815ff3d4546
Hardik203 <153900500+Hardik203@users.noreply.github.com>
2024-10-22 19:33:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import SignupForm from './components/SignupForm';
-import Navbar from './components/Navbar'; // Import Navbar
+
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />  {/* Include Navbar */}
-      <SignupForm />
-    </div>
+  <>
+  </>
   );
 }
 
