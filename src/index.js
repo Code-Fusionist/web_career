9e17a48660690cd3680309bc12e84133776ffbe8
Hardik203 <153900500+Hardik203@users.noreply.github.com>
2024-10-22 19:31:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./screen/Home"
-import Pred from "./screen/Pred";
+import Pred from "./screen/Prediction";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home/>}></Route>
-        <Route path="/pred" element={<Pred/>}></Route>
+        <Route path="/pred" element={<Prediction/>}></Route>
       </Routes>
     </BrowserRouter>
   );
