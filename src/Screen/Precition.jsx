fe63899a5f5e16cfe6c54ba835b8f00789847d91
Hardik203 <153900500+Hardik203@users.noreply.github.com>
2024-10-22 12:30:21 +0530

    Create Precition.jsx

---
+++ src/Screen/Precition.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Prediction() {
+  return (
+    <>
+    <div className='bg-gray-100'>
+<h1>Hello wordl</h1>
+      <h2>This is prediction page</h2>
+    </div>
+    </>
+  )
+}
