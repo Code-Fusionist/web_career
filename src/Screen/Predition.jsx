27470eda67391e8c18c4412704d825a63c1a8daf
Hardik203 <153900500+Hardik203@users.noreply.github.com>
2024-10-22 19:35:20 +0530

    Update Predition.jsx

--- src/Screen/Predition.jsx
+++ src/Screen/Predition.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Navbar from "./components/Navbar";
 export default function Prediction() {
   return (
     <>
     <div className='bg-gray-100'>
+      <Navbar/>
 <h1>Hello wordl</h1>
       <h2>This is prediction page</h2>
     </div>
