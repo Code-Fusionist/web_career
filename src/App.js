7a8a326b94532f9d5f093853a46f9edde4bb82b0
KESHAV AGGARWAL <153336129+Code-Fusionist@users.noreply.github.com>
2024-10-24 14:54:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React from "react";
 // import SignupForm from "./components/SignupForm";
 // import Navbar from "./components/Navbar";
 // import Carousel from "./components/Carousel";
-import Login from "./components/Login";
+// import Login from "./components/Login";
+// import DropdownMenu from "./components/DropdownMenu";
+// import LoginForm from "./components/loginGlassmorphism";
+// import HoverGlassmorphism from "./components/HoverGlassmorpism";
+// import SquareMenu from "./components/SquareMenu";
+import Enigma from "./components/Enigma";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      {/* <Navbar /> */}
+      {/* <DropdownMenu /> */}
+      {/* <Login /> */}
+      {/* <LoginForm /> */}
+      {/* <HoverGlassmorphism /> */}
+      <Enigma />
+      {/* <SquareMenu /> */}
     </div>
   );
 }
